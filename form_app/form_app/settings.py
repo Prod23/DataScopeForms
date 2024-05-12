6081df655b00ab481dcddbfe76a54b6ab18536fd
Prod23 <88609959+Prod23@users.noreply.github.com>
2024-05-12 20:14:07 +0530

    Update allowed host

--- form_app/form_app/settings.py
+++ form_app/form_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8^^6znr)szg9x!a_7_9*gcnn-7i$4ipy=zw5w+@o*d)=x^f(mm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
